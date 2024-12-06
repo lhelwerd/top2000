973739ab3b21d90dce0cdd41cf7adbebba343c05
Leon Helwerda <leon.helwerda@gmail.com>
2024-12-06 17:54:53 +0100

    Cleanup JSON dump and add schema
    - Keep keys in separate dump entry to keep the track properly typed and
      not too verbose when inspecting it.
    - Replace some internal track data names with better field names based
      on TOML setting.
    - Only keep track data that's stored for a field name or year position
    - Convert year/position fields to numbers, drop empty strings and zeros
    - Add current year to dump
    - Skip JSON dump from schema check on source input data
    - Perform schema check of JSON dump if it exists

--- top2000/output/json.py
+++ top2000/output/json.py
@@ -5,7 +5,7 @@ JSON dump output.
 import json
 from pathlib import Path
 from .base import Format
-from ..readers.base import Base as ReaderBase
+from ..readers.base import Base as ReaderBase, Key
 
 class JSON(Format):
     """
@@ -24,12 +24,34 @@ class JSON(Format):
         reverse = self._get_bool_setting(output_format, "reverse")
         self.reset()
 
+        track_keys: list[list[Key]] = []
         tracks = []
         positions = []
+        fields = self._get_dict_setting(output_format, "fields")
+        numeric_fields = {"year"}
+        fields.update({
+            str(year): str(year)
+            for year in range(self._first_year, self._current_year)
+        })
+        numeric_fields.update({
+            str(year) for year in range(self._first_year, self._current_year)
+        })
+
         for position, keys in self._sort_positions(data.positions, reverse):
             self._check_position(position, reverse)
             track = data.tracks[keys[0]]
-            track["keys"] = keys
+            track = {
+                field: track[key] for key, field in fields.items()
+                if key in track
+            }
+            for field in numeric_fields:
+                if field in track:
+                    if track[field] in {"", "0", 0}:
+                        track.pop(field)
+                    else:
+                        track[field] = int(track[field])
+
+            track_keys.append(keys)
             tracks.append(track)
             positions.append(position)
 
@@ -37,7 +59,9 @@ class JSON(Format):
             json.dump({
                 "tracks": tracks,
                 "positions": positions,
-                "artists": data.artists
+                "keys": track_keys,
+                "artists": data.artists,
+                "year": self._current_year
             }, json_file)
 
         return True
--- validate_schema.sh
+++ validate_schema.sh
@@ -7,7 +7,7 @@ echo "Validating schemas in schema against metaschema"
 check-jsonschema --check-metaschema schema/*.json
 
 echo "Validating JSON API responses"
-check-jsonschema --schemafile schema/top2000.json *.json
+check-jsonschema --schemafile schema/top2000.json top2000-*.json
 
 echo "Validating fields.toml"
 check-jsonschema --schemafile schema/fields.json fields.toml
@@ -17,3 +17,8 @@ check-jsonschema --schemafile schema/fixes.json fixes.toml
 
 echo "Validating output.toml"
 check-jsonschema --schemafile schema/output.json output.toml
+
+if ls output-*.json 1>/dev/null 2>&1; then
+	echo "Validating output-*.json"
+	check-jsonschema --schemafile schema/dump.json output-*.json
+fi
