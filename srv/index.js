5e27ff03e3b887e2063c09b3fa4e4299b9c470b8
Leon Helwerda <leon.helwerda@gmail.com>
2024-12-15 00:22:09 +0100

    HTML viewer: Initial work on progression chart
    Clicking a row will add an informative panel below it, currently
    displaying just a chart with positions in earlier years with a curved
    line which becomes disjointed if the track has left the chart during
    some years. Each year's position is marked with a point, current year is
    marked with a star.

--- srv/index.js
+++ srv/index.js
@@ -10,6 +10,7 @@ const nl = d3.timeFormatLocale({
     shortMonths: ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"]
 });
 const formatTime = nl.format("%d %b %H:%M");
+const formatYear = nl.format("'%y");
 
 const data = await d3.json("output-sorted.json"); // jshint ignore: line
 const firstYear = data.first_year;
@@ -54,7 +55,7 @@ const formatRankChange = (d, position) => {
         }
         return "\u21c4";
     }
-    for (let year = currentYear - 2; year >= 1999; year--) {
+    for (let year = currentYear - 2; year >= data.first_year; year--) {
         if (year in d) {
             return `\u27f2${year}`;
         }
@@ -94,11 +95,81 @@ table.append("thead").append("tr").selectAll("th")
     .data(columns)
     .join("th")
     .text(d => d.column);
-table.append("tbody").selectAll("tr")
+const rows = table.append("tbody").selectAll("tr")
     .data(data.tracks)
     .join("tr")
-    .each(setCurrent)
-    .selectAll("td")
+    .each(setCurrent);
+rows.on("click", function(event, d) {
+    const next = d3.select(this.nextSibling);
+    if (next.classed("is-info")) {
+        next.remove();
+        return;
+    }
+    const pos = d3.select(this.firstChild).datum();
+    const cell = d3.select(this.parentNode).insert("tr", () => this.nextSibling)
+        .classed("is-info", true)
+        .append("td")
+        .attr("colspan", "4");
+
+    // Progression chart
+    const width = 640;
+    const height = 400;
+    const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 40;
+    const lastDate = new Date(data.year, 0);
+    const x = d3.scaleUtc()
+        .domain([new Date(data.first_year, 0), lastDate])
+        .range([marginLeft, width - marginRight]);
+    const y = d3.scaleLinear()
+        .domain([data.positions[data.positions.length - 1], data.positions[0]])
+        .range([height - marginBottom, marginTop]);
+    const xTicks = x.ticks(data.year - data.first_year);
+    xTicks.push(lastDate);
+    const yTicks = y.ticks(10);
+    yTicks.push(data.positions[0]);
+    const line = d3.line()
+        .defined(d => typeof d[1] !== "undefined")
+        .x(d => x(d[0]))
+        .y(d => y(d[1]))
+        .curve(d3.curveMonotoneX);
+    const xAxis = d3.axisBottom(x)
+        .tickValues(xTicks)
+        .tickFormat(formatYear);
+    const svg = cell.append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(xAxis);
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y).tickValues(yTicks));
+    const linePath = svg.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 2);
+    const years = [];
+    for (let year = data.first_year; year < data.year; year++) {
+        const date = new Date(year, 0);
+        years.push([date, d[year]]);
+        if (year in d) {
+            svg.append("circle")
+                .attr("fill", "black")
+                .attr("r", 4)
+                .attr("cx", x(date))
+                .attr("cy", y(d[year]));
+        }
+    }
+    years.push([lastDate, data.positions[pos]]);
+    svg.append("path")
+        .attr("fill", "black")
+        .attr("d", d3.symbol(d3.symbolStar))
+        .attr("transform", `translate(${x(lastDate)}, ${y(data.positions[pos])})`);
+    linePath.attr("d", line(years));
+});
+rows.selectAll("td")
     .data((d, i) => Array(columns.length).fill(i))
     .join("td")
     .text((pos, i) => columns[i].field(data.tracks[pos], data.positions[pos]));
